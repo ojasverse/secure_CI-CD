f92d27bdaf845914957f752cf3e848f3725f6903
ojaswi <ojaswi1903@gmail.com>
2026-03-19 14:52:55 +0000

    app

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
 console.log("hello secure ci/cd");
 const AWS_SECRET_ACCESS_KEY = "AKIAIOSFODNN7EXAMPLE";
+const password = "MyHardcodedPassword123";
+const AWS_SECRET_ACCESS_KEY = "AKIAIOSFODNN7EXAMPLE";
